12873c40e3c81b5d2ce12572349db6b8786ffb3b
Sergii Kozakov <sergiikozakov.co@gmail.com>
2023-06-09 05:11:49 +0300

    Tapping Into Context with the useContext Hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../context/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -43,6 +44,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authCxt = useContext(AuthContext);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -93,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCxt.onLogin(emailState.value, passwordState.value);
   };
 
   return (
